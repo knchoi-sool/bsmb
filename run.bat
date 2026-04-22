f3bb22e6b17be9d072f13a4ad6fb09e630a1c297
최경남 <ckn@soolsool.co.kr>
2026-04-22 11:21:57 +0900

    run.bat git 정보 조회 방식 개선: PowerShell 사용, CRLF 변환

--- run.bat
+++ run.bat
@@ -1,14 +1,9 @@
 @echo off
 title BSM Dashboard Server
 
-:: Git 정보 (임시파일 방식 - 오류 없음)
-git rev-parse --short HEAD > "%TEMP%\git_hash.tmp" 2>nul
-set /p GIT_HASH=<"%TEMP%\git_hash.tmp"
-del "%TEMP%\git_hash.tmp" 2>nul
-
-git rev-parse --abbrev-ref HEAD > "%TEMP%\git_branch.tmp" 2>nul
-set /p GIT_BRANCH=<"%TEMP%\git_branch.tmp"
-del "%TEMP%\git_branch.tmp" 2>nul
+:: Git ???
+for /f %%a in ('powershell -NoProfile -Command "git rev-parse --short HEAD"') do set GIT_HASH=%%a
+for /f %%a in ('powershell -NoProfile -Command "git rev-parse --abbrev-ref HEAD"') do set GIT_BRANCH=%%a
 
 echo.
 echo  =====================================================
